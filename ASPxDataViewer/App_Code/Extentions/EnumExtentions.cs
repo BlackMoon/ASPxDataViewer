434164ea9685a68d18b00caebe1895aca1e1c5af
rrv <admin@mail.ru>
2016-02-08 20:22:42 +0300

    Order + EnumExtensions

---
+++ ASPxDataViewer/App_Code/Extentions/EnumExtentions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Extentions
+{
+    /// <summary>
+    /// Расширение Enum
+    /// </summary>
+    public static class EnumExtensions
+    {
+
+        /// <summary>
+        /// Получить аттрибуты типа
+        /// </summary>
+        /// <typeparam name="T">тип</typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static T GetAttribute<T>(this Enum value) where T : Attribute
+        {
+            Type type = value.GetType();
+            MemberInfo [] memberInfo = type.GetMember(value.ToString());
+            object [] attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
+
+            if (attributes.Length > 0)
+                return (T) attributes[0];
+            
+            return null;
+        }
+
+
+        /// <summary>
+        /// Переводит Enum в строку с учетом аттрибута [Description] (если задан)
+        /// </summary>
+        /// <typeparam name="T">тип Enum</typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToName<T>(this Enum value)
+        {
+            DescriptionAttribute attribute = value.GetAttribute<DescriptionAttribute>();
+            return attribute == null ? Enum.GetName(typeof(T), value) : attribute.Description;
+        }
+    }
+}
---
+++ ASPxDataViewer/App_Code/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/// <summary>
+/// Заказ
+/// </summary>
+[Serializable]
+public class Order
+{
+    /// <summary>
+    /// Код
+    /// </summary>
+    public int Code { get; set; }
+
+    /// <summary>
+    /// Описание
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Количество
+    /// </summary>
+    public decimal Amount { get; set; }
+
+    /// <summary>
+    /// Цена
+    /// </summary>
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// Статус
+    /// </summary>
+    public ObjectState State { get; set; }
+}
